30a368b3178afa57f969f452c21f3e08b7011080
Szymon Kamiński <szym.kaminski@gmail.com>
2025-03-08 21:40:06 +0100

    Updated file paths

--- Infogain_final.py
+++ Infogain_final.py
@@ -30,8 +30,8 @@ def run_query(sql_query):
 
 
 # downloading data
-ds1=pd.read_excel(r"C:\Python files\Infogain dataset\dataset1.xlsm")
-ds2=pd.read_excel(r"C:\Python files\Infogain dataset\dataset2.xlsm")
+ds1=pd.read_excel(r"InfoGainTest/dataset1.xlsm")
+ds2=pd.read_excel(r"InfoGainTest/dataset1.xlsm")
 
 con = sqlite3.connect("patients.db")
 ds1.to_sql("healthinfo", con, index=False, if_exists="replace")
